c2f5880ee37b827f79f74b36311e03ec56df8a5a
afdhal-PC\afdhal <awakenblueheart@gmail.com>
2017-07-02 09:59:42 +0800

    no message

--- nodejs_project/mindspacetutorial/nodejswithexpressjs2_handlebars/app.js
+++ nodejs_project/mindspacetutorial/nodejswithexpressjs2_handlebars/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
